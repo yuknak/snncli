9f7d233c9d1adfe94c061b56727ab27fe3299528
yuknak <yuknak4@gmail.com>
2020-12-14 18:22:49 +0900

    Test impl. of refresh control of List

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,77 @@
 
+////////////////////////////////////////////////////////////////////////////////
+
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Container, Content, Text,List,ListItem } from 'native-base';
 import HomeHeader from './HomeHeader'
+import { Alert, RefreshControl,View } from "react-native";
 
-class Line extends Component {
-  render() {
-    return (
-      <ListItem>
-        <Text>Home</Text>
-      </ListItem>
-    )
+////////////////////////////////////////////////////////////////////////////////
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false
+    }
   }
-
-}
-export default class Home extends Component {
+  componentDidMount() {
+    //Alert.alert('test')
+  }
+  componentWillUnmount(){
+  }
   render() {
+    const items=['test1','test2','test3','test4']
     return (
       <Container>
       <HomeHeader {...this.props} />
-      <Content>
-          <List>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-            <Line/>
-          </List>
-          </Content>
+        <View>
+          <List
+            refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={()=>{
+                this.setState({refreshing: true})}
+            } /> }
+            dataArray={items}
+            renderRow={(item) =>
+              <ListItem>
+                <Text>{item}</Text>
+              </ListItem>
+            }
+            keyExtractor={(item, index) => item.toString()}
+            />
+          </View>
         </Container>
     )
   }
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+const mapStateToProps = state => {
+  return {
+    apiState: state.apiState,
+    appState: state.appState,
+    uiState: state.uiState,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    api: (params,success,error) =>
+      dispatch(apiState.api(params,success,error)),
+    selectMarket: (market) =>
+      dispatch(uiState.selectMarket(market)),
+    selectOrder: (market, order) =>
+      dispatch(uiState.selectOrder(market, order)),
+    showAlert: (variant, message) =>
+      dispatch(uiState.showAlert(variant, message)),
+  }
+}
+////////////////////////////////////////////////////////////////////////////////
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
+
+////////////////////////////////////////////////////////////////////////////////
